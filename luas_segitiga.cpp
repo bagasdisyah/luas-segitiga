0877c6e909b2875b805aad1908c48d78cad688cc
bagasdisyah <144640778+bagasdisyah@users.noreply.github.com>
2023-09-11 09:18:21 +0700

    Create luas_segitiga.cpp

---
+++ luas_segitiga.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main ( ) {
+int a,t,L;
+    cout << " Menghitung Luas Segitiga \n"; 
+    cout<<"masukan Alas: ";
+    cin >>a;
+    cout<<"Masukan tinggi:";
+    cin >>t;
+    L=0.5*a*t;
+    cout<<"Hasil Luas Segitiga adalah: "<<L<< endl;
+}
